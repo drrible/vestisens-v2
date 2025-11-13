a40f087931e68cdcac01f71e8f404d37bc94d625
drrible <asdasdarqw3123123q05@mail.ru>
2025-11-13 15:26:07 +0200

    New Commit

--- server.php
+++ server.php
@@ -10,7 +10,6 @@
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
 );
-
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
@@ -19,3 +18,4 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
 }
 
 require_once __DIR__.'/public/index.php';
+?>
