944ff2e867a6499ddf4978f3f58d94a82f4e2b51
drrible <asdasdarqw3123123q05@mail.ru>
2026-02-13 21:43:13 +0200

    Commit

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -12,17 +12,9 @@ class AdminMiddleware
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        if(Auth::check() && Auth::user()->role == 2)
-        {
-
-            return $next($request);
-        }
-
-        abort(404);
-
-    }
+      /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
 }
