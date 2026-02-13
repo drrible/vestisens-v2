4472dbe81701a3011bc21d7755c8b4cba5ec275b
drrible <asdasdarqw3123123q05@mail.ru>
2026-02-13 21:45:47 +0200

    categories

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
-//use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\AdminAuth   // removed for testing category
 
 class TestsController extends Controller
 {
